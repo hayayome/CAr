a5578df3a0ed528ea7d3fe6387a48ff990700f35
hayayome <116540273+hayayome@users.noreply.github.com>
2023-10-22 22:04:08 +0900

    1차 코딩

---
+++ 자동차 슈퍼 레이스/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace 자동차_슈퍼_레이스
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.BtnPause = new System.Windows.Forms.Label();
+            this.BtnStart = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbxCar = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.3765F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.6235F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(834, 672);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.lbltime);
+            this.panel2.Controls.Add(this.BtnPause);
+            this.panel2.Controls.Add(this.BtnStart);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(665, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(166, 666);
+            this.panel2.TabIndex = 1;
+            // 
+            // lbltime
+            // 
+            this.lbltime.AutoSize = true;
+            this.lbltime.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbltime.Location = new System.Drawing.Point(50, 428);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(60, 23);
+            this.lbltime.TabIndex = 1;
+            this.lbltime.Text = "00:00";
+            // 
+            // BtnPause
+            // 
+            this.BtnPause.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnPause.Location = new System.Drawing.Point(21, 469);
+            this.BtnPause.Name = "BtnPause";
+            this.BtnPause.Size = new System.Drawing.Size(119, 61);
+            this.BtnPause.TabIndex = 1;
+            this.BtnPause.Text = "Pause";
+            this.BtnPause.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BtnStart.Location = new System.Drawing.Point(21, 573);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(119, 61);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "게임시작";
+            this.BtnStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pbxCar);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(656, 666);
+            this.panel1.TabIndex = 0;
+            this.panel1.TabStop = true;
+            // 
+            // pbxCar
+            // 
+            this.pbxCar.Image = global::자동차_슈퍼_레이스.Properties.Resources.자동차;
+            this.pbxCar.Location = new System.Drawing.Point(275, 469);
+            this.pbxCar.Name = "pbxCar";
+            this.pbxCar.Size = new System.Drawing.Size(83, 150);
+            this.pbxCar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxCar.TabIndex = 0;
+            this.pbxCar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(834, 672);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxCar)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label BtnPause;
+        private System.Windows.Forms.Label BtnStart;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbxCar;
+        private System.Windows.Forms.Label lbltime;
+    }
+}
+
---
+++ 자동차 슈퍼 레이스/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 자동차_슈퍼_레이스
+{
+    public partial class Form1 : Form
+    {
+        private int timer = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyDown += Form1_KeyDown;
+
+            BtnStart.MouseHover += BtnPause_MouseHover;
+            BtnStart.MouseLeave += BtnPause_MouseLeave;
+            BtnStart.Click += BtnStart_Click;
+
+            BtnPause.MouseHover += BtnPause_MouseHover;
+            BtnPause.MouseLeave += BtnPause_MouseLeave;
+            BtnPause.Click += BtnPause_Click;
+
+            timer1.Tick += timer_Tick;
+            timer1.Interval = 1000;
+
+            List<Image> Hurdles = new List<Image>()
+            {
+                Properties.Resources.정지,
+                Properties.Resources.스쿨존,
+                Properties.Resources.주차금지,
+                Properties.Resources.표지판
+            };
+        }
+
+        private void BtnPause_MouseHover(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button button = (Button)sender;
+                button.BackColor = Color.Red;
+            }
+        }
+
+        private void BtnPause_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Button)
+            {
+                Button button = (Button)sender;
+                button.BackColor = Color.Transparent;
+            }
+        }
+
+        //private void BtnStart_MouseLeave(object sender, EventArgs e)
+        //{
+        //    BtnStart.BackColor = Color.Transparent;
+        //}
+
+        //private void BtnStart_MouseHover(object sender, EventArgs e)
+        //{
+        //    BtnStart.BackColor = Color.SkyBlue;
+        //}
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    pbxCar.Left += 5;
+                    break;
+                case Keys.Left:
+                    pbxCar.Left -= 5;
+                    break;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            timer++;
+            UpdateTimeLabel();
+        }
+
+        private void BtnPause_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void UpdateTimeLabel()
+        {
+            int minutes = timer / 60;
+            int second = timer % 60;
+            lbltime.Text = $"{minutes:D2}:{second:D2}";
+        }
+    }
+}
